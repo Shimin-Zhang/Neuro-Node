144cf1bbe291af82ee6b9c0849cdfcc942451658
John Zhang <john.shimin.zhang@gmail.com>
2016-02-14 23:20:34 -0800

    add parsing for asic information

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ const PARSER_SYNC_BYTE = 0xAA  /* Syncronization byte */
 const PARSER_EXCODE_BYTE = 0x55  /* EXtended CODE level byte */
 
 /* Extend Codes */
+const BATTER_LEVEL = 0x01 /* Batter Level */
 const POOR_SIGNAL_QUALITY = 0x02 /* Poor signal quality 1 byte */
 const HEART_RATE = 0x03 /* Heart Rate 1 byte */
 const ATTENTION= 0x04 /* Attention 1 byte */
 const MEDITATION = 0x05 /* Meditation 1 byte */
-const EIGHT_BIT_RAW = 0x06 /* 8 bite raw value 1 byte */
+const EIGHT_BIT_RAW = 0x06 /* 8 bit raw value 1 byte */
+const RAW_16_BIT_VALUE= 0x80 /* 16 bit raw value signed */
+const EEG_POWER = 0x81 /* 32 byte for 8 wave types */
+const ASIC_EEG_POWER = 0x83 /* 24 byte for 8 wave types */
+const PRINTERVAL = 0x86 /* 2 byte representing miliseonds betwee r peaks */
+
 
 function parsePayload(payload) {
     let data = {};
@@ -23,6 +29,11 @@ function parsePayload(payload) {
     while (pos < payload.length - 1) {
         let cursor = payload[pos];
         switch(cursor) {
+            case(BATTER_LEVEL):
+                console.log('found battery level', payload[pos + 1])
+                data.batteryLevel = payload[pos + 1];
+                pos+=2;
+                break;
             case(POOR_SIGNAL_QUALITY):
                 data.signalQuality = payload[pos + 1];
                 pos+=2;
@@ -34,18 +45,42 @@ function parsePayload(payload) {
                 break;
             case(ATTENTION):
                 console.log('found attention', payload[pos + 1])
-                data.heartRate = payload[pos + 1];
+                data.attention = payload[pos + 1];
                 pos+=2;
                 break;
             case(MEDITATION):
                 console.log('found meditation', payload[pos + 1])
-                data.heartRate = payload[pos + 1];
+                data.meditation = payload[pos + 1];
                 pos+=2;
                 break;
             case(EIGHT_BIT_RAW):
                 console.log('found raw data', payload[pos + 1])
-                data.heartRate = payload[pos + 1];
+                data.raw8 = payload[pos + 1];
                 pos+=2;
+                break;
+            case(RAW_16_BIT_VALUE):
+                const raw16 = payload.slice(pos + 1, pos + 3);
+                console.log('found signed 8 bit data', raw16)
+                data.raw16 = raw16;
+                pos+=3;
+                break;
+            case(EEG_POWER):
+                const eegPower = payload.slice(pos + 1, pos + 33);
+                console.log('found eeg power', eegPower)
+                data.eeg = eegPower;
+                pos+=33;
+                break;
+            case(ASIC_EEG_POWER):
+                const asicEEG = payload.slice(pos + 1, pos + 25);
+                console.log('found ASIC EEG', asicEEG)
+                data.asicEEG = asicEEG;
+                pos+=25;
+                break;
+            case(PRINTERVAL):
+                const prInterval = payload.slice(pos + 1, pos + 3);
+                console.log('found ASIC EEG', prInterval)
+                data.rinterval = prInterval;
+                pos+=3;
                 break;
             default:
                 pos+=1;
