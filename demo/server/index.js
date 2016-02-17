40f453266f091a4d42fc8948ea4553ec1d344b65
John Zhang <john.shimin.zhang@gmail.com>
2016-02-17 01:15:32 -0800

    add charting demo

--- demo/server/index.js
+++ demo/server/index.js
@@ -10,17 +10,53 @@ function startSocket() {
             'attention': 'attention',
             'meditation': 'meditation',
             'raw8': '',
-            'asicEEG': 'asic-eeg'
         }
         let data = JSON.parse(message.data);
-        console.log(message);
         Object.keys(data).forEach((key) => {
-            if (mapping[key]) {
-                const id = mapping[key];
-                document.querySelector('#' + id).innerHTML = data[key];
-            }
         });
+        if (data.eegValues) {
+            updateChartData(data.eegValues);
+        }
     };
 }
-
+function updateChartData(data) {
+    ['delta', 'theta', 'lowAlpha','highAlpha', 'lowBeta', 'highBeta', 'lowGamma', 'midGamma'].forEach((name) => {
+        let dataArray = window.eegData.find((eeg) => {
+            return eeg.name === name;
+        }).data;
+        dataArray.push(data[name]);
+    });
+    window.chart.setData(window.eegData).render();
+}
+function createChart() {
+        window.eegData = [
+            {name: 'delta', data: []},
+            {name: 'theta', data: []},
+            {name: 'lowAlpha', data: []},
+            {name: 'highAlpha', data: []},
+            {name: 'lowBeta', data: []},
+            {name: 'highBeta', data: []},
+            {name: 'lowGamma', data: []},
+            {name: 'midGamma', data: []},
+        ];
+        window.chart = new Contour({
+                el: '.line-chart',
+                xAxis: {
+                    type: 'linear'
+                },
+                yAxis: {
+                    title: 'value'
+                },
+                legend: {
+                    vAlign: 'top',
+                    hAlign: 'left'
+                }
+            })
+            .cartesian()
+            .line(window.eegData)
+            .legend(window.eegData)
+            .tooltip()
+            .render();
+}
 document.addEventListener('DOMContentLoaded', startSocket);
+document.addEventListener('DOMContentLoaded', createChart);
